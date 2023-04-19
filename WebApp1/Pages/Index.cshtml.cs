aafc873db2d7ad6f01993e464276f845728994a1
Travis-DOH <124600138+Travis-DOH@users.noreply.github.com>
2023-04-19 21:18:03 +0000

    Added more analysis of copilot responses

--- WebApp1/Pages/Index.cshtml.cs
+++ WebApp1/Pages/Index.cshtml.cs
@@ -18,3 +18,51 @@ namespace WebApp1.Pages
         }
     }
 }
+
+// create a new class called colorSort
+public class colorSort
+{
+    public string color { get; set; }
+    public int count { get; set; }
+
+    public colorSort(string color, int count)
+    {
+        this.color = color;
+        this.count = count;
+    }
+
+    public override string ToString()
+    {
+        return color + " " + count;
+    }
+
+    public static List<colorSort> SortColors(List<colorSort> colors)
+    {
+        List<colorSort> sortedColors = new List<colorSort>();
+        int count = 0;
+        string color = "";
+        foreach (colorSort c in colors)
+        {
+            if (c.color == color)
+            {
+                count += c.count;
+            }
+            else
+            {
+                if (color != "")
+                {
+                    sortedColors.Add(new colorSort(color, count));
+                }
+                color = c.color;
+                count = c.count;
+            }
+        }
+        sortedColors.Add(new colorSort(color, count));
+        return sortedColors;
+    }
+}
+
+
+
+
+
